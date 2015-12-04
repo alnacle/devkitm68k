ab2be0ed1ad7c7d288eaa266b2647e2daa3716f9
Alvaro Navarro <anavarro@achtung.es>
2015-12-04 18:38:00 +0100

    Add rc file for setting up global environment. Add ld linker based on DevKit

---
+++ config.mk
@@ -0,0 +1,9 @@
+
+CC = ${NGPREFIX}-gcc
+AR = ${NGPREFIX}-ar
+AS = ${NGPREFIX}-as
+LD = ${NGPREFIX}-ld
+RANLIB = ${NGPREFIX}-ranlib 
+OBJCOPY = ${NGPREFIX}-objcopy
+BIN2O = bin2elf 
+FIXCNV = tiletool
