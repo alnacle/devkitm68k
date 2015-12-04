44f04f5b22a9aa9cba53028a26b7c88418c8370e
Alvaro Navarro <anavarro@achtung.es>
2015-12-04 18:41:58 +0100

    Update config.mk with cosmetic changes

--- config.mk
+++ config.mk
@@ -1,9 +1,9 @@
-
+# toolchain binaries
 CC = ${NGPREFIX}-gcc
 AR = ${NGPREFIX}-ar
 AS = ${NGPREFIX}-as
 LD = ${NGPREFIX}-ld
 RANLIB = ${NGPREFIX}-ranlib 
 OBJCOPY = ${NGPREFIX}-objcopy
-BIN2O = bin2elf 
-FIXCNV = tiletool
+BIN2O = bin2elf
+
